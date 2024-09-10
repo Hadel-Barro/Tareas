08c66b229b218ac039e36cff330a18c488f69118
Javiera Navarro <javiera.navarro.p@mail.pucv.cl>
2024-09-09 22:52:16 -0300

    Primer commit

---
+++ Tarea 1,3.py
@@ -0,0 +1,6 @@
+while True:
+    user_input = input("Por favor escribe algo: ") 
+    if user_input.lower() == "salir":
+        print("Finalizando el programa, gracias por su tiempo.")
+        break
+    
